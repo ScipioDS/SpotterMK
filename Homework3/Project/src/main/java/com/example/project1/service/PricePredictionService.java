08a628c7310ac6f67131c2cd73f68a2b6fe9285b
aleksandrakrusharoska <137446966+aleksandrakrusharoska@users.noreply.github.com>
2024-12-29 20:02:54 +0100

    Update PricePredictionService.java

--- Homework3/Project/src/main/java/com/example/project1/service/PricePredictionService.java
+++ Homework3/Project/src/main/java/com/example/project1/service/PricePredictionService.java
@@ -1,6 +1,5 @@
 package com.example.project1.service;
 
-import com.example.project1.controller.CompanyController;
 import com.example.project1.entity.HistoricalDataEntity;
 import com.example.project1.repository.HistoricalDataRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.data.domain.Pageable;
+
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,17 +34,22 @@ public class PricePredictionService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        //List<HistoricalDataEntity> data = historicalDataRepository.findByCompanyIdAndDateBetween(companyId, LocalDate.now().minusMonths(3), LocalDate.now());
-        // Fetch the last 30 entries
-        Pageable pageable = PageRequest.of(0, 100); // Page 0, 30 entries
-        List<HistoricalDataEntity> data = historicalDataRepository.findTop30ByCompanyIdOrderByDateDesc(companyId, pageable);
+        List<HistoricalDataEntity> data = historicalDataRepository.findByCompanyIdAndDateBetween(companyId, LocalDate.now().minusYears(1), LocalDate.now());
+        if (data.size() >= 150) {
+            data = data.stream()
+                    .sorted(Comparator.comparing(HistoricalDataEntity::getDate).reversed())
+                    .limit(150)
+                    .collect(Collectors.toList());
+        }
+
+//      Pageable pageable = PageRequest.of(0, 100);     // Fetch the last entries
+//      List<HistoricalDataEntity> data = historicalDataRepository.findHistoricalDataByCompanyId(companyId, pageable);
 
         Map<String, Object> requestBody = Map.of("data", mapToRequestData(data));
         logger.info("Prediction request payload: {}", requestBody);
-        // Send the payload to the prediction service (use your existing API call)
+        logger.info("Request Body: {}", requestBody);
 
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
-
         Map<String, Double> response = restTemplate.postForObject(predictionApiUrl, requestEntity, Map.class);
 
         return response != null ? response.get("predicted_next_month_price") : null;
